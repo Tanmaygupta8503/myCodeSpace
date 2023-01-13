56e2321eab5518e5f76d457017cc2acf0a1805c9
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2023-01-13 19:12:56 +0530

    DFS Graph Printing

--- graphs/Graphs.java
+++ graphs/Graphs.java
@@ -1,3 +1,40 @@
+package graphs;
+import java.util.Scanner;
+/*
+				  DFS
+				  printing
+				  in a graph
+				  
+				     0     3
+				    / \     \
+				   1   2     4
+				   
+				   input:	output:
+				   
+				   n = 5	0
+				   e = 4	1
+				   0 1		2
+				   1 2		3
+				   0 2		4
+				   3 4
+				   
+ */
+public class Graphs {
+	public static void printHelper(int edge[][] , int sv , boolean visited[])
+	{
+		System.out.println(sv);
+		visited[sv] = true;
+		int n = edge.length;
+		for(int i = 0 ;i < n ; i ++) {
+			if(! visited[i] && edge[sv][i] == 1) {
+				printHelper(edge , i ,visited);
+			}
+		}
+	}
+	public static void print (int edge[][]) {
+		
+		boolean visited[] = new boolean[edge.length];
+		for(int i = 0 ; i < edge.length ; i ++) {
 			if(! visited[i])
 			printHelper(edge , i , visited);
 		}
